f207430675304b2511b61db4178cd600a7eef0a5
iftakhar <alamshuvozeny019@gmail.com>
2023-11-15 10:47:24 +0600

    object destructing

--- destracturing.js
+++ destracturing.js
@@ -1,1 +1,27 @@
-g
+// object destracturin 
+const user ={
+    name:"iftakhar",
+    id:3753772239,
+    roll:2255532,
+    age:25,
+    education:{
+        degree:"hons",
+
+    }
+}
+// object tekhe name takae ber kore ene r akta variable rakthe hobe jeno onno jagai use korti pari
+// let another =user["name"];
+// console.log(another);
+// eta hocche normally amra distracture na kore jevabe kortam kintu jokhon es6 cole ase tokhon life ta r o easy hoye jai 
+
+// const {roll:ExamRoll} = user;
+// console.log(ExamRoll);
+
+//eta hosse es6 e distracturing er akdom basic
+
+
+//nested object 
+// const {education:{degree}={}}=user;
+// console.log(degree);
+//benge pelar upai hocce last laveel porjonto same vabe jete hobe .
+//default value use kore porperty eroor komano jai 
