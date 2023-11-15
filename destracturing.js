e4592ad5a1c9e575381ac3b3e7247f9d8d90ae4a
iftakhar <alamshuvozeny019@gmail.com>
2023-11-15 11:06:10 +0600

    array destracturing

--- destracturing.js
+++ destracturing.js
@@ -25,3 +25,38 @@ const user ={
 // console.log(degree);
 //benge pelar upai hocce last laveel porjonto same vabe jete hobe .
 //default value use kore porperty eroor komano jai 
+
+
+
+// array destructing 
+
+// let number = [1,2,3,6,4,5];
+// let [a,b]=number;
+// console.log(a,b);
+
+//array destructing er basic 
+// const numbers=[1,2,3,4,5,6,7];
+// const [, x,,,,, y]=numbers ;
+// console.log(x,y);
+
+//jokhon random numbers need hobe tkn evabe kaj kora jabe
+
+//jokhon nested array takhbe tokhon evabe korte hobe
+// const another=[1,2,[1,500,1000],7,8];
+// const [,,[,z,s]]=another;
+// console.log(z,s);
+
+//value swaping
+//old way
+let a=10;
+let b=20;
+
+// var temp=a;
+// var a=b;
+// var b =temp;
+// console.log(a,b);
+
+
+// es6 way 
+[b,a]=[a,b];
+console.log(a,b);
